66d51de1d1f5b6f0b877db6013e65f8f6e609917
nyoongoon <nyoongoon@naver.com>
2024-05-23 08:09:12 +0900

    first commit

---
+++ src/main/java/org/example/lecturskimyounghan/LectursKimYoungHanApplication.java
@@ -0,0 +1,13 @@
+package org.example.lecturskimyounghan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LectursKimYoungHanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LectursKimYoungHanApplication.class, args);
+    }
+
+}
