4be04e972d22d7b743a4e47b7c544e57e059dd07
nyoongoon <nyoongoon@naver.com>
2024-05-27 07:09:28 +0900

    클래스 & 기본형과 참조형

---
+++ java-basic/src/main/java/org/example/class1/ClassStart3.java
@@ -0,0 +1,8 @@
+package org.example.class1;
+
+public class ClassStart3 {
+    public static void main(String[] args) {
+        Student student1;
+        student1 = new Student(); // 메모리에 실제 데이터를 올린다.
+    }
+}
